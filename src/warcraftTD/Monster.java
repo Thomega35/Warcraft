b488f6a61640c137446016eb382418be2244800f
Gpinault <63723955+SireS0mbre@users.noreply.github.com>
2020-12-11 14:14:47 +0100

    .

--- src/warcraftTD/Monster.java
+++ src/warcraftTD/Monster.java
@@ -1,15 +1,15 @@
 package warcraftTD;
 
 public abstract class Monster {
-	// Position du monstre à l'instant t
+	// Position du monstre ﾃ� l'instant t
 	Position p;
 	// Vitesse du monstre
 	double speed;
-	// Position du monstre à l'instant t+1
+	// Position du monstre ﾃ� l'instant t+1
 	Position nextP;
-	// Boolean pour savoir si le monstre à atteint le chateau du joueur
+	// Boolean pour savoir si le monstre ﾃ� atteint le chateau du joueur
 	boolean reached;
-	// Compteur de déplacement pour savoir si le monstre à atteint le chateau du joueur
+	// Compteur de deplacement pour savoir si le monstre ﾃ� atteint le chateau du joueur
 	int checkpoint = 0;
 	
 	public Monster(Position p) {
@@ -18,14 +18,14 @@ public abstract class Monster {
 	}
 	
 	/**
-	 * Déplace le monstre en fonction de sa vitesse sur l'axe des x et des y et de sa prochaine position.
+	 * Dﾃｩplace le monstre en fonction de sa vitesse sur l'axe des x et des y et de sa prochaine position.
 	 */
 	public void move() {
 		// Mesure sur quel axe le monstre se dirige.
 		double dx = nextP.x - p.x;
 		double dy = nextP.y - p.y;
 		if (dy + dx != 0){
-			// Mesure la distance à laquelle le monstre à pu se déplacer.
+			// Mesure la distance ﾃ� laquelle le monstre ﾃ� pu se dﾃｩplacer.
 			double ratioX = dx/(Math.abs(dx) + Math.abs(dy));
 			double ratioY = dy/(Math.abs(dx) + Math.abs(dy));
 			p.x += ratioX * speed;
@@ -40,7 +40,7 @@ public abstract class Monster {
 	}
 	
 	/**
-	 * Fonction abstraite qui sera instanciée dans les classes filles pour afficher le monstre sur le plateau de jeu.
+	 * Fonction abstraite qui sera instanciﾃｩe dans les classes filles pour afficher le monstre sur le plateau de jeu.
 	 */
 	public abstract void draw();
 }
